a2fca99c2f68ad5ceca1b5f0cc18e6b57cf62b20
Darsh374 <dp594@njit.edu>
2020-04-10 14:38:18 -0400

    task: all files and functions added for population sampling

---
+++ PopulationSampling/SimpleRandom.py
@@ -0,0 +1,7 @@
+import random
+
+def simpleRandomSample(population, numSelect):
+    sampleList = []
+    random.shuffle(population)
+    sampleList = random.sample(population, numSelect)
+    return sampleList
